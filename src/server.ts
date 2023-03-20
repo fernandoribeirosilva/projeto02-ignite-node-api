891ecee450c7b192f25e2ca5727cb56d3d27deef
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2023-03-20 16:20:56 -0300

    configurando variáveis de anbiente

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,23 @@
 import fastify from 'fastify'
+// import crypto from 'node:crypto'
 import { knex } from './database'
 
 const server = fastify()
 
 server.get('/ping', async (req, res) => {
-  const tables = await knex('sqlite_schema').select('*')
+  // const transaction = await knex('transactions')
+  //   .insert({
+  //     id: crypto.randomUUID(),
+  //     title: 'Transação de teste',
+  //     amount: 1000,
+  //   })
+  //   .returning('*')
 
-  return tables
+  const transaction = await knex('transactions')
+    .where('amount', 500)
+    .select('*')
+
+  return transaction
 })
 
 server
