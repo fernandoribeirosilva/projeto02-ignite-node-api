39ec31a6b8c82334bcd4add69ea62b523e247ee6
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2023-03-21 17:45:59 -0300

    tratando env com zod

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify'
 // import crypto from 'node:crypto'
 import { knex } from './database'
+import { env } from './env'
 
 const server = fastify()
 
@@ -22,7 +23,7 @@ server.get('/ping', async (req, res) => {
 
 server
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP sever Running!')
