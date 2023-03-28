24c65e10eaefbf68098e8c8315a6c078a8ebd1a8
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2023-03-28 16:50:20 -0300

    criado teste para a criação de transações

--- src/test/transactions.spec.ts
+++ src/test/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { test, beforeAll, afterAll } from 'vitest'
+import { beforeAll, afterAll, describe, it } from 'vitest'
 import request from 'supertest'
 
 import { app } from '../app'
@@ -9,21 +9,23 @@ import { app } from '../app'
 // para executar depois de cada testes afterEach
 // para executar depois de todos os testes afterAll
 
-beforeAll(async () => {
-  await app.ready()
-})
+describe('Transaction routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
 
-afterAll(async () => {
-  await app.close()
-})
+  afterAll(async () => {
+    await app.close()
+  })
 
-test('user can create a new transaction', async () => {
-  await request(app.server)
-    .post('/transactions')
-    .send({
-      title: 'New transaction',
-      amount: 5000,
-      type: 'credit',
-    })
-    .expect(201)
+  it('user can create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
 })
