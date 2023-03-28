46f69a246e2ad0c498e71945c77bfd4ccc755e44
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2023-03-28 10:47:40 -0300

    Testando a criação de transações

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,6 @@
-import fastify from 'fastify'
-import cookie from '@fastify/cookie'
-
 import { env } from './env'
-import { transactionsRoutes } from './routes/transactions'
 
-const server = fastify()
-
-server.register(cookie)
-
-server.addHook('preHandler', async (request) => {
-  console.log(`[${request.method}] ${request.url}]`)
-})
-
-server.register(transactionsRoutes, {
-  prefix: 'transactions',
-})
-
-server.get('/ping', async () => {
-  console.log({ pong: true })
-})
+import { app as server } from './app'
 
 server
   .listen({
--- src/test/transactions.spec.ts
+++ src/test/transactions.spec.ts
@@ -1,7 +1,29 @@
-import { expect, test } from 'vitest'
+import { test, beforeAll, afterAll } from 'vitest'
+import request from 'supertest'
 
-test('o usuário consegue criar uma nova transação', () => {
-  const responseStatusCode = 201
+import { app } from '../app'
 
-  expect(responseStatusCode).toEqual(201)
+// vai executar algum código antes que todos os testes executem
+// esta função só vai executar apenas uma única vez, antes de todos os testes
+// para antes de cada testes deve usar beforeEach
+// para executar depois de cada testes afterEach
+// para executar depois de todos os testes afterAll
+
+beforeAll(async () => {
+  await app.ready()
+})
+
+afterAll(async () => {
+  await app.close()
+})
+
+test('user can create a new transaction', async () => {
+  await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'New transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+    .expect(201)
 })
